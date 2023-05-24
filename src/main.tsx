9da33730402485e57bc7b516acca1fb160ae095a
Anna Burdanova <burdanovaas@gmail.com>
2023-05-24 15:31:50 +0700

    feat: :construction: Add admin's dashboard component mockup
    Combined and restyled multiple tailwind components into a single component. No functionality yet, only styles.

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 
 import Signup from "./pages/Signup.tsx";
 import Signin from "./pages/Signin.tsx";
+import Dashboard from "./pages/AdminDashboard.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <Dashboard />
     <Signup />
     <Signin />
   </React.StrictMode>
