5c44a7c05d7408f25436434b5d8750520e46cf16
Anna Burdanova <burdanovaas@gmail.com>
2023-05-25 14:14:43 +0700

    feat: Add User Dashboard Static Component

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,28 @@ import "./index.css";
 import Signup from "./pages/Signup.tsx";
 import Signin from "./pages/Signin.tsx";
 import Dashboard from "./pages/AdminDashboard.tsx";
+import UserDashboard from "./pages/UserDashboard.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <div className="flex h-[5vh] flex-col justify-end bg-yellow-300 text-lg font-bold">
+      <h1> User's dashboard preview:</h1>
+    </div>
+    <UserDashboard />
+
+    <div className="flex h-[10vh] flex-col justify-end bg-yellow-300 text-lg font-bold">
+      <h1> Admin's dashboard preview:</h1>
+    </div>
     <Dashboard />
+
+    <div className="flex h-[10vh] flex-col justify-end bg-yellow-300 text-lg font-bold">
+      <h1> Sign up page preview:</h1>
+    </div>
     <Signup />
+
+    <div className="flex h-[10vh] flex-col justify-end bg-yellow-300 text-lg font-bold">
+      <h1> Login page preview:</h1>
+    </div>
     <Signin />
   </React.StrictMode>
 );
