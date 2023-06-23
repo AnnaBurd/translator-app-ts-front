3c650afd1124da9d1fc4df8c91d53e4b17d84b5c
Anna Burdanova <burdanovaas@gmail.com>
2023-06-23 16:28:08 +0700

    feat: :construction: add basic interactivity animations to sign in form

--- src/auth/useSignin.ts
+++ src/auth/useSignin.ts
@@ -17,31 +17,27 @@ const useSignin = () => {
   const from = location.state?.from?.pathname || "/";
 
   const signin = async (user: User) => {
-    try {
-      const response = await fetch(`${Config.API_BASE_URL}/users/signin`, {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify(user),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
+    const response = await fetch(`${Config.API_BASE_URL}/users/signin`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
 
-      if (!response.ok) {
-        throw new Error("Could not sign in");
-      }
-
-      const json = await response.json();
-
-      updateAccessToken(json.accessToken);
-      updateUserDetails(json.data);
-
-      // Go to previous location from location state, or to user dash by default
-      navigate(from, { replace: true });
-    } catch (error) {
-      console.log("😶‍🌫️", error);
+    if (!response.ok) {
+      throw new Error("Could not sign in");
     }
+
+    const json = await response.json();
+
+    updateAccessToken(json.accessToken);
+    updateUserDetails(json.data);
+
+    // Go to previous location from location state, or to user dash by default
+    navigate(from, { replace: true });
   };
 
   return signin;
--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import OAuth from "../../auth/OAuth";
 import useSignin from "../../auth/useSignin";
 import AnimatedPage from "../../components/animations/AnimatedPage";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Signin() {
   const signin = useSignin();
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
 
-    signin({ email: "test@mail.com", password: "111" });
+    try {
+      setIsLoading(true);
+      // signin({ email: "test@mail.com", password: "111" });
+      // setIsLoading(false);
+    } catch (error) {
+      navigate("/error");
+    }
   };
 
   return (
@@ -27,10 +38,42 @@ export default function Signin() {
 
           <form
             action=""
-            className="mb-0 mt-4 space-y-4 rounded-lg border border-slate-200 p-4 shadow-lg sm:p-6 lg:p-8"
+            className="relative mb-0 mt-4  overflow-hidden rounded-lg border border-slate-200 p-4 shadow-lg sm:p-6 lg:p-8"
             onSubmit={handleSubmit}
           >
-            <div>
+            {isLoading && (
+              <motion.div
+                className="absolute left-0 top-0 z-30 h-[20rem] w-[40rem] bg-[--color-secondary]"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.5, transition: { duration: 1 } }}
+              ></motion.div>
+            )}
+            {isLoading && (
+              <motion.span
+                className="absolute left-1/2 top-1/3 z-50 -translate-x-1/2"
+                initial={{ opacity: 0 }}
+                animate={{
+                  // rotate: 360,
+                  opacity: 1,
+                  transition: { delay: 0.6, duration: 0.5 },
+                  // transition: { rotate: { duration: 2, yoyo: Infinity } },
+                }}
+              >
+                <div aria-label="Loading..." role="status">
+                  <svg className="h-7 w-7 animate-spin" viewBox="3 3 18 18">
+                    <path
+                      className="fill-[--color-secondary]"
+                      d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                    ></path>
+                    <path
+                      className="fill-[--color-primary]"
+                      d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+                    ></path>
+                  </svg>
+                </div>
+              </motion.span>
+            )}
+            <div className="mb-4 mt-4">
               <label htmlFor="email" className="sr-only">
                 Email
               </label>
@@ -62,7 +105,7 @@ export default function Signin() {
               </div>
             </div>
 
-            <div>
+            <div className="mb-4">
               <label htmlFor="password" className="sr-only">
                 Password
               </label>
@@ -101,7 +144,7 @@ export default function Signin() {
             </div>
             <button
               type="submit"
-              className="block w-full rounded-md border border-indigo-400 bg-indigo-400 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-indigo-400 focus:outline-none focus:ring active:text-indigo-300"
+              className="mb-4 block w-full rounded-md border border-indigo-400 bg-indigo-400 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-indigo-400 focus:outline-none focus:ring active:text-indigo-300"
             >
               Sign in
             </button>
