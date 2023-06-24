f0e2f2d4588211a52c6f4f2c4f4ab456b492ddc0
Anna Burdanova <burdanovaas@gmail.com>
2023-06-24 16:00:08 +0700

    feat: :construction: add placeholder for silent signin loading animation

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import AuthContext from "./AuthContext";
 import { useContext } from "react";
 import useSilentSignin from "./useSilentSignin";
+import { AnimatePresence, motion } from "framer-motion";
 
 /**
  * Parent element for private react-router-dom routes. Checks if user has signed in, otherwise attempts a silent sign in / redirects to sign up route.
@@ -15,9 +16,23 @@ const RequireAuth = () => {
 
   return (
     <>
-      {isRunningSilentSignin && (
-        <div>TODO: Wait a little bit, page is loading (render circle) </div>
-      )}
+      <AnimatePresence>
+        {isRunningSilentSignin && (
+          <motion.div
+            className="absolute left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-[--color-secondary] text-2xl font-bold text-red-500"
+            initial={{ opacity: 0, x: 0 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{
+              opacity: 0,
+              // y: -500,
+              transition: { ease: "easeIn", duration: 0.3 },
+            }}
+            transition={{ duration: 10 }}
+          >
+            <motion.div>Silent Sign in is RUNNING ...</motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       {!isRunningSilentSignin &&
         (signedInUser ? (
           <Outlet />
