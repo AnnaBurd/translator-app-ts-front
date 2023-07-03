f36e105fde3ed06cf7cbd6b7f214823b18f99e86
Anna Burdanova <burdanovaas@gmail.com>
2023-07-03 15:25:59 +0700

    feat: :construction: add password hide-reveal option

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -26,6 +26,8 @@ const inputValidationSchema = yup
   .required();
 
 export default function Signin() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   const signin = useSignin();
   // const navigate = useNavigate();
 
@@ -56,6 +58,10 @@ export default function Signin() {
       setErrorSigningIn("Could not signin with these credentials.");
       // navigate("/error");
     }
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
   };
 
   return (
@@ -230,7 +236,7 @@ export default function Signin() {
               <div className="relative">
                 <input
                   {...register("password")}
-                  type="password"
+                  type={passwordVisible ? "text" : "password"}
                   className={`w-full rounded-lg border-slate-200 p-4 pe-12 text-sm shadow-sm transition-colors duration-300 focus-within:outline-1 ${
                     errors.password
                       ? "text-rose-600 focus-within:outline-rose-400"
@@ -241,29 +247,59 @@ export default function Signin() {
                   aria-invalid={errors.password ? "true" : "false"}
                 />
 
-                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`h-4 w-4 ${
-                      errors.password ? "text-rose-400" : "text-slate-400"
-                    }`}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    />
-                  </svg>
+                <span
+                  className="absolute inset-y-0 end-0 grid cursor-pointer place-content-center px-4"
+                  onClick={togglePasswordVisibility}
+                >
+                  {!passwordVisible && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-4 w-4 ${
+                        errors.password ? "text-rose-400" : "text-slate-400"
+                      }`}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  )}
+
+                  {passwordVisible && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-4 w-4 ${
+                        errors.password ? "text-rose-400" : "text-slate-400"
+                      }`}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  )}
                 </span>
               </div>
               {errors.password && (
